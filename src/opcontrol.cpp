4cf00547dbbceddec09cee67ffb0979d4076c4fe
rj14ng <rocco.jiang5@gmail.com>
2019-09-19 17:27:39 +0800

    Add intial drive code with tank control (untested)

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -14,19 +14,25 @@ using namespace okapi;
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
-void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-		int left = master.get_analog(ANALOG_LEFT_Y);
-		int right = master.get_analog(ANALOG_RIGHT_Y);
 
-		left_mtr = left;
-		right_mtr = right;
-		pros::delay(20);
+const int DRIVE_MOTOR_LEFT_F = 2;    // Front left drive motor
+const int DRIVE_MOTOR_LEFT_B = 11;   // Back left drive motor
+const int DRIVE_MOTOR_RIGHT_F = 3;   // Front right drive motor
+const int DRIVE_MOTOR_RIGHT_B = 12;  // Back right drive motor
+
+auto chassis = ChassisControllerFactory::create(
+	{-DRIVE_MOTOR_LEFT_F, -DRIVE_MOTOR_LEFT_B},  // Left motors
+	{DRIVE_MOTOR_RIGHT_F, DRIVE_MOTOR_RIGHT_B}   // Right motors
+);
+
+void opcontrol()
+{
+	Controller controller;
+
+	while (true)
+	{
+		// Tank drive with left and right sticks
+		chassis.tank(controller.getAnalog(ControllerAnalog::leftY),
+							 controller.getAnalog(ControllerAnalog::rightY));
 	}
 }
